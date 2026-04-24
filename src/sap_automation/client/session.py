20a61caf7e1a89a741877aa6766360939464d2ca
Adao Oliveira <adaum79@gmail.com>
2026-04-24 14:24:09 -0300

    feat: melhorias em Session e vkeys

--- src/sap_automation/client/session.py
+++ src/sap_automation/client/session.py
@@ -2,6 +2,7 @@ import logging
 import time
 
 from sap_automation.core.retry import retry
+from sap_automation.types import VKEY
 
 
 class SAPSession:
@@ -12,11 +13,11 @@ class SAPSession:
     # ----------------------------------
     # CORE
     # ----------------------------------
-
     @retry(max_attempts=3, delay=0.5)
     def find(self, path: str):
         return self._session.findById(path)
 
+    # -------
     def exists(self, path: str) -> bool:
         try:
             self.find(path)
@@ -27,29 +28,51 @@ class SAPSession:
     # ----------------------------------
     # INTERAÇÕES
     # ----------------------------------
-
     def get_text(self, path: str) -> str:
         return self.find(path).text
 
+    # -------
     def set_text(self, path: str, value: str):
         self.find(path).text = value
 
+    # -------
+    def get_radio(self, path: str) -> bool:
+        return self.find(path).selected
+
+    # -------
+    def set_radio(self, path: str):
+        self.find(path).select()
+
+    # -------
     def press(self, path: str):
         self.find(path).press()
 
+    # -------
     def send_vkey(self, key: int):
         self.find("wnd[0]").sendVKey(key)
 
     # ----------------------------------
     # TRANSAÇÃO
     # ----------------------------------
-
     def start_transaction(self, code: str):
         self.set_text("wnd[0]/tbar[0]/okcd", code)
+        self.send_vkey(VKEY["Enter"])
+
+    # -------
+    def end_transaction(self):
+        self.send_vkey(VKEY["F3"])
+
+    # -------
+    def send_command(self, command: str):
+        """
+        Executa comando SAP (/n, /o, /i, etc.)
+        """
+        self.find("wnd[0]/tbar[0]/okcd").text = command
         self.send_vkey(0)
 
-    def end_transaction(self):
-        self.send_vkey(3)
+    # -------
+    def go_home(self):
+        self.send_command("/n")
 
     # ----------------------------------
     # STATUS BAR
---
+++ src/sap_automation/types/__init__.py
@@ -0,0 +1,1 @@
+from .vkeys import VKEY
